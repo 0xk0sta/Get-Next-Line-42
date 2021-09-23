077401931783d29ce0c4c763de9b693b83b80459
acostal- <acostalcardenas@gmail.com>
2021-09-23 20:14:22 +0200

    first commit

---
+++ get_next_line.c
@@ -0,0 +1,124 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acostal- <acostal-@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/23 20:03:55 by acostal-          #+#    #+#             */
+/*   Updated: 2021/09/23 20:03:58 by acostal-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlcat(char *dest, const char *src,
+					 size_t dstsize)
+{
+	size_t	i;
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	dst_len_og;
+
+	i = 0;
+	dst_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	dst_len_og = ft_strlen(dest);
+	if (dstsize == 0)
+		return (src_len);
+	if (dstsize < dst_len_og)
+		return (src_len + dstsize);
+	while (dst_len < dstsize - 1 && src[i] != '\0')
+	{
+		dest[dst_len] = src[i];
+		i++;
+		dst_len++;
+	}
+	dest[dst_len] = '\0';
+	return (dst_len_og + src_len);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	if (c == '\0')
+		return ((char *)&s[ft_strlen(s)]);
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	return ((char *) '\0');
+}
+
+static void	read_file(int fd, char *buffer, char **file, ssize_t r_bytes)
+{
+	char	*tmp_str;
+
+	tmp_str = NULL;
+	while (r_bytes > 0)
+	{
+		buffer[r_bytes] = '\0';
+		if (!(*file))
+			(*file) = ft_strdup("");
+		tmp_str = ft_strjoin((*file), buffer);
+		free((*file));
+		(*file) = tmp_str;
+		if (ft_strchr(buffer, '\n'))
+			break ;
+		r_bytes = read(fd, buffer, BUFFER_SIZE);
+	}
+	free(buffer);
+}
+
+static char	*return_next_line(char **s)
+{
+	char	*output_str;
+	char	*tmp_str;
+	size_t	len;
+
+	len = 0;
+	output_str = NULL;
+	while ((*s)[len] != '\n' && (*s)[len])
+		len++;
+	if ((*s)[len] == '\n')
+	{
+		output_str = ft_substr(*s, 0, len + 1);
+		tmp_str = ft_strdup(*s + len + 1);
+		free(*s);
+		*s = tmp_str;
+		if (!**s)
+		{
+			free(*s);
+			*s = NULL;
+		}
+		return (output_str);
+	}
+	output_str = ft_strdup(*s);
+	free(*s);
+	*s = NULL;
+	return (output_str);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*buffer;
+	static char	*file = 0;
+	ssize_t		r_bytes;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	r_bytes = read(fd, buffer, BUFFER_SIZE);
+	read_file(fd, buffer, &file, r_bytes);
+	if (r_bytes < 0)
+		return (NULL);
+	if (!r_bytes && (!file || !*file))
+		return (NULL);
+	return (return_next_line(&file));
+}
---
+++ get_next_line_utils.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acostal- <acostal-@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/23 20:05:52 by acostal-          #+#    #+#             */
+/*   Updated: 2021/09/23 20:05:54 by acostal-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strdup(const char *src)
+{
+	char	*dst;
+	int		size;
+	int		index;
+
+	index = 0;
+	size = ft_strlen(src);
+	dst = malloc(sizeof(char) * (size + 1));
+	if (dst == NULL)
+		return (0);
+	while (src[index])
+	{
+		dst[index] = src[index];
+		index++;
+	}
+	dst[index] = '\0';
+	return (dst);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*new_str;
+	int		total_len;
+	int		index;
+
+	if (!s1 || !s2)
+		return (NULL);
+	total_len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	index = 0;
+	new_str = malloc(sizeof(char) * total_len);
+	if (!new_str)
+		return (NULL);
+	while (s1[index])
+	{
+		new_str[index] = s1[index];
+		index++;
+	}
+	new_str[index] = '\0';
+	ft_strlcat(new_str, s2, total_len);
+	return (new_str);
+}
+
+size_t	ft_strlcpy(char *dest, const char *src,
+					 size_t dstsize)
+{
+	size_t	i;
+
+	i = 0;
+	if (dstsize == 0)
+		return (ft_strlen(src));
+	while (i < dstsize - 1 && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (ft_strlen(src));
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*new_str;
+
+	if (!s)
+		return (NULL);
+	if (ft_strlen(s) < len)
+		len = ft_strlen(s);
+	new_str = malloc((sizeof(char) * len) + 1);
+	if (start > ft_strlen(s))
+	{
+		*new_str = 0;
+		return (new_str);
+	}
+	if (new_str == NULL)
+		return (NULL);
+	if (start <= ft_strlen(s))
+		ft_strlcpy(new_str, s + start, len + 1);
+	return (new_str);
+}
